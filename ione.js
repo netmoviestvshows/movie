08b87a40ba23504c1679196a3c907ac8f71c9ffb
netmoviestvshows <100079575+netmoviestvshows@users.noreply.github.com>
2022-07-11 13:23:00 +0700

    reupdate

--- ione.js
+++ ione.js
@@ -1,3 +1,5 @@
+<script type='text/javascript'>
 var u=document.URL;
 u=u.replace(&#39;https://iwan-handsome.blogspot.com/&#39;, &#39;https://str15.besttvmovie.com/&#39;);
 location.href = u;
+</script>
