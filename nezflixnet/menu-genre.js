6fa33160cab9a98d97bd8e3d6d9992894fece7d7
netmoviestvshows <100079575+netmoviestvshows@users.noreply.github.com>
2024-09-25 02:52:45 +0700

    Update menu-genre.js

--- nezflixnet/menu-genre.js
+++ nezflixnet/menu-genre.js
@@ -1,6 +1,6 @@
 // script tampil di semua kecuali halaman genre.html
-        var app = angular.module("movieApp", []);
-        app.controller("movieController", function ($scope, $http) {
+        var app = angular.module("mtApp", []);
+        app.controller("mtController", function ($scope, $http) {
             const apiKey = "a79576e54c5bbb893011b98ca48f2460";
             $scope.genres = [];
 
