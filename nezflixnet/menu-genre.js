f722e84b587bd654ae02935f4457c2ac9e83bbb5
netmoviestvshows <100079575+netmoviestvshows@users.noreply.github.com>
2024-09-25 02:14:32 +0700

    Create menu-genre.js

---
+++ nezflixnet/menu-genre.js
@@ -0,0 +1,18 @@
+// script tampil di semua kecuali halaman genre.html
+        var app = angular.module("movieApp", []);
+        app.controller("movieController", function ($scope, $http) {
+            const apiKey = "a79576e54c5bbb893011b98ca48f2460";
+            $scope.genres = [];
+
+            // Mengambil daftar genre dari TMDb
+            $http.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`)
+                .then(function (response) {
+                    $scope.genres = response.data.genres;
+                });
+
+            // Fungsi untuk mengubah genre saat mengklik genre
+            $scope.changeGenre = function (genreName) {
+                const formattedGenreName = genreName.toLowerCase().replace(/\s+/g, "-");
+                window.location.href = `/p/genre.html?genre=${formattedGenreName}`;
+            };
+        });
